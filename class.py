75a57f61aea1edb367f612be0d5d3c57ac9aaae1
fatihroy <afroyhan1@gmail.com>
2022-01-13 08:20:29 +0700

    init 2

--- class.py
+++ class.py
@@ -1,10 +1,10 @@
 # create class
 class Item:
     pembayaran = 0.5 #pembayaran setelah diskon 50%
+    produk = []
     # init akan di kerjakan otomatis ketika classnya dipanggil
                    #mengubah tipe data | nilai default
     def __init__(self, name:str, price:float, quantity=0):
-        print(f"The name is :{name}")
         # membuat aturan value bahwa price & quantity harus >=0
         assert price >= 0, f"{price} is not equal to or greater than 0" #menambah pesan kesalahan
         assert quantity >= 0, f"{quantity} is not equal to or greater than 0"
@@ -12,27 +12,27 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
-        
+        # menambahkan item
+        Item.produk.append(self)
+
     def calculate_total_price(self): #ini adalah method
         return self.price*self.quantity #gak perlu parameters karena sudah tau
 
     def diskon(self):
         # self.price = self.price * Item.pembayaran (dikali denagn Item.pembayaran)
         self.price = self.price * self.pembayaran # (dikali denagn self.pembayaran)
-item1 = Item('book', 5)
-# item1.name = "book" bisa didelete krn sudah dideklarasikan di self.name
-# item1.price = 10
-# item1.quantity = 5
-item1.diskon()
-print(item1.price)
 
-item2 = Item('pen', 10, 100) 
-item2.pembayaran = 0.2 #membuat pembayaran sendiri untuk item2
-item2.diskon()
-print(item2.price)
-# print(Item.__dict__) #menunjukkan seluruh atribut di Item
-# print(item1.__dict__) #menunjukkan seluruh atribut di item1
-# print(item2.__dict__) #menunjukkan seluruh atribut di item2
-# print(Item.pembayaran)
-# print(item1.calculate_total_price())
-# print(item2.calculate_total_price())
+    def __repr__(self):
+        # menampilkan secara otomatis dlm bentuk arraynya produk
+        return f"item('{self.name}',{self.price},{self.quantity}) "
+        
+item1 = Item('Letter', 10, 100)
+item2 = Item('Hvs', 5, 100)
+item3 = Item('Bag', 7, 100)
+item4 = Item('Pen', 10, 100)
+item5 = Item('book', 1, 10)
+
+# menampilkan item1-item5 satu-satu
+# for i in Item.produk:
+#     print(i.name)
+print(Item.produk)
