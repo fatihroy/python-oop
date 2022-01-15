b255c1e8ee887192f1859c52b2c81363de4a30ca
fatihroy <afroyhan1@gmail.com>
2022-01-15 08:45:36 +0700

    getters dan setters

---
+++ Item.py
@@ -0,0 +1,74 @@
+# Getters and Setters
+
+import csv
+from logging import exception #untuk csv
+# mengambil data dari csv
+
+class Item:
+    pembayaran = 0.5 #pembayaran setelah diskon 50%
+    product = []
+    # init akan di kerjakan otomatis ketika classnya dipanggil
+                   #mengubah tipe data | nilai default
+    def __init__(self, name:str, price:float, quantity=0):
+        # membuat aturan value bahwa price & quantity harus >=0
+        assert price >= 0, f"{price} is not equal to or greater than 0" #menambah pesan kesalahan
+        assert quantity >= 0, f"{quantity} is not equal to or greater than 0"
+        # meng-assingn object
+        # self.name = name tidak boleh seperti ini karena ada property
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        
+        # menambahkan item
+        Item.product.append(self)
+    
+    @property
+    # property decorator = Hanya Membaca di atribut
+    def name(self): #supaya kalau ketik self.name maka hasilnya adalah self.name dan tidak bisa diubah
+        print('anda mencoba mendapatkan name')
+        return self.__name
+
+    @name.setter #meng-set value baru untuk name
+    def name(self, val):
+        print("anda mencoba mengubah name")
+        # apabila val nya lebih dari 10 karakter maka raise exception
+        if len(val) > 10:
+            raise exception("name nya terlalu panjang")
+        else:
+         self.__name = val
+
+    def calculate_total_price(self): #ini adalah method
+        return self.price*self.quantity #gak perlu parameters karena sudah tau
+
+    def diskon(self):
+        # self.price = self.price * Item.pembayaran (dikali denagn Item.pembayaran)
+        self.price = self.price * self.pembayaran # (dikali denagn self.pembayaran)
+
+    @classmethod
+    def ambil_dariCSV(cls):
+        with open('item.csv', 'r') as L: #ambil dari item.csv 
+            reader = csv.DictReader(L) #supaya data di item.csv terbaca
+            items = list(reader) #dijadikan list
+
+        # tampilkan satu persatu
+        for item in items:
+          
+            Item(
+                # mengambil nama, harga, kuantitas
+                name = item.get('nama'),
+                # mengubah jadi int krn semua yang ada di csv adalah string
+                price = float(item.get('harga')),
+                quantity = int(item.get('kuantitas')),
+            )
+
+    
+    
+    def __repr__(self):
+        # menampilkan secara otomatis dlm bentuk arraynya product
+  # self.__class__.__name__ = fungsi untuk memanggil nama class untuk arraynya product
+        return f"{self.__class__.__name__}('{self.name}',{self.price},{self.quantity}) "
+
+    # @property (hanya bisa dibaca ktk dipanggil)
+    # def readOnlyName(self):
+    #     return "AAA"
+        
--- class.py
+++ class.py
@@ -1,76 +1,15 @@
-# inherritance
+from Item import Item
 
-import csv #untuk csv
-# mengambil data dari csv
-# create class
-class Item:
-    pembayaran = 0.5 #pembayaran setelah diskon 50%
-    product = []
-    # init akan di kerjakan otomatis ketika classnya dipanggil
-                   #mengubah tipe data | nilai default
-    def __init__(self, name:str, price:float, quantity=0):
-        # membuat aturan value bahwa price & quantity harus >=0
-        assert price >= 0, f"{price} is not equal to or greater than 0" #menambah pesan kesalahan
-        assert quantity >= 0, f"{quantity} is not equal to or greater than 0"
-        # meng-assingn object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        
-        # menambahkan item
-        Item.product.append(self)
+item1 = Item('Item1', 313)
+item1.name = 'item 244444ff' #bisa diubah karena ada setternya
+# item1.name = 'item2' akan error
+print(item1.name)
+# item1.readOnlyName = "FFFF" akan error karena kita gak bisa set attribute dari readOnlyName
 
-    def calculate_total_price(self): #ini adalah method
-        return self.price*self.quantity #gak perlu parameters karena sudah tau
+# akses Class Item
+# Item.ambil_dariCSV()
+# print(Item.product)
 
-    def diskon(self):
-        # self.price = self.price * Item.pembayaran (dikali denagn Item.pembayaran)
-        self.price = self.price * self.pembayaran # (dikali denagn self.pembayaran)
-
-    @classmethod
-    def ambil_dariCSV(cls):
-        with open('item.csv', 'r') as L: #ambil dari item.csv 
-            reader = csv.DictReader(L) #supaya data di item.csv terbaca
-            items = list(reader) #dijadikan list
-
-        # tampilkan satu persatu
-        for item in items:
-          
-            Item(
-                # mengambil nama, harga, kuantitas
-                name = item.get('nama'),
-                # mengubah jadi int krn semua yang ada di csv adalah string
-                price = float(item.get('harga')),
-                quantity = int(item.get('kuantitas')),
-            )
-
-    
-    
-    def __repr__(self):
-        # menampilkan secara otomatis dlm bentuk arraynya product
-  # self.__class__.__name__ = fungsi untuk memanggil nama class untuk arraynya product
-        return f"{self.__class__.__name__}('{self.name}',{self.price},{self.quantity}) "
-        
-
-# membuat inherritance spy phone mewarisi class Item
-class Phone(Item):
-    
-    def __init__(self, name:str, price:float, quantity=0, broken=0):
-        # pemanggilan ke super function untuk bisa akses semua attribut dari Item
-        super().__init__(
-          name, price, quantity
-        )
-       
-        
-        # membuat aturan value bahwa price & quantity harus >=0
-        assert broken >= 0, f"{quantity} is not equal to or greater than 0"
-
-        # meng-assingn object
-        self.broken = broken
 
        
-
-phone1 = Phone("Han12x", 100, 5, 1)     
-print(Item.product)
-print(Phone.product)
 
